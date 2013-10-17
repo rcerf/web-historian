719eea8b05c6be4293fb969113a5082dcc12e0bf
rcerf <rickcerf@gmail.com>
2013-10-17 12:06:33 -0700

    Completed helper functions

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,9 @@
+var helpers = require('./html-fetcher-helpers.js');
+var fs = require('fs');
+
+// Every 1 minute, do the following with cron:
+helpers.readUrls(path.join(__dirname, "../data/sites.txt"), helpers.downloadUrls);
+
 // eventually, you'll have some code here that uses the tested helpers 
 // to actually download the urls you want to download.
+
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,32 @@
+var fs = require('fs');
+var http = require('http-get');
+
+// Reads sites.txt and places each URL in that file into a callback
+// In the spec, this callback places the URLs into an array.
+// Our usage: readUrls is called from htmlfetcher with downloadUrls as the cb if the file has not yet been downloaded.
 exports.readUrls = function(filePath, cb){
+  var urls = [];
 
+  var fileData = fs.readFileSync(filePath, {encoding:'utf8'});
+
+  urls = fileData.split('\n');
+
+  cb(urls);
 };
 
+// Given an array of URLs, grab the html at each one.
+// Writing this html to disk at data/sites in the proper file (e.g. html for example2.com goes into data/sites/www.example2.com)
 exports.downloadUrls = function(urls){
-  // fixme
+
+  if (urls) {
+    for (var i = 0; i < urls.length; i++) {
+      console.log("Downloading %s", urls[i]);
+      http.get({url: urls[i]}, '../data/sites/' + urls[i]);
+    }
+  }
+  return true;
 };
+
+
+
+
